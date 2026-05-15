941e7edfc6ac84c0b2c4d469467840846a796071
jmtrurod <jmtrurod@gmail.com>
2026-05-15 22:26:19 +0200

    Requested prompt: `TBD` (u) src/main/java/com/pepeai/pokedrawings/service/NumberService.java (+) src/test/java/com/pepeai/pokedrawings/service/NumberServiceTest.java

--- src/main/java/com/pepeai/pokedrawings/service/NumberService.java
+++ src/main/java/com/pepeai/pokedrawings/service/NumberService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class NumberService {
 
-    private int currentNumber = 0;
+    private int currentNumber = 99;
 
     public int getNextNumber() {
         currentNumber++;
